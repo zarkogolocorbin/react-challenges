0b1b914311cbe3527bdd34fa3c71dd3cab6c898a
zarko <zarkogolocorbin@gmail.com>
2023-06-21 12:29:36 +0200

    add first project

---
+++ 01-pizza-menu/src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <h1>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti iusto
+      cum earum reprehenderit dolores consequatur minima dicta reiciendis
+      voluptatibus accusamus aut error quo aperiam, officia tenetur illo ullam
+      voluptate delectus.
+    </h1>
+  );
+}
+
+export default App;
