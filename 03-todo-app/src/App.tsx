ec9d55521c535e181a617e98faedb95872687eeb
zarko <zarkogolocorbin@gmail.com>
2023-06-22 12:48:26 +0200

    create third react challenge project

---
+++ 03-todo-app/src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <h1 className="text-red-500">
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magnam
+      repellendus itaque voluptatem, reiciendis possimus voluptas? Excepturi
+      porro explicabo alias nam ex expedita rerum magni, quam quidem nisi, non
+      et laudantium?
+    </h1>
+  );
+}
+
+export default App;
